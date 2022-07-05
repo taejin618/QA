7ae700ecd947198d7b563849c6d682dba302f3f7
taejin618 <105828871+taejin618@users.noreply.github.com>
2022-07-05 15:52:35 +0900

    Update and rename katalonpipeline.groovy to docker katalon pipeline.groovy

---
+++ docker katalon pipeline.groovy
@@ -0,0 +1,29 @@
+
+
+pipeline {
+    agent any
+        stages {
+        stage('Test') {
+            steps {
+                dir('C:\\Users\\taejin618\\katalonstudio\\test'){
+                    bat 'docker run -t --rm  -m 4096m --cpus=2  -v %CD%:/tmp/project katalonstudio/katalon katalonc.sh -projectPath=/tmp/project -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/suite" -apiKey=f2cc6a94-2a4a-4435-b15a-9c0911989ada'
+                }
+            }
+        }
+    }
+}
+
+
+
+pipeline {
+    agent any
+        stages {
+        stage('Test') {
+            steps {
+                dir('C:\\Users\\taejin618\\katalonstudio\\test'){
+                    bat 'docker run -t --rm  -m 4096m --cpus=2  -v %CD%:/tmp/project katalonstudio/katalon katalonc.sh -projectPath=/tmp/project -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/suite" -apiKey=f2cc6a94-2a4a-4435-b15a-9c0911989ada'
+                }
+            }
+        }
+    }
+}
