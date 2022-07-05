92668aa7863cfe6bb71ab699ae339255bf77a0d2
taejin618 <105828871+taejin618@users.noreply.github.com>
2022-07-05 15:05:08 +0900

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,3 +1,5 @@
+//카탈론 런타임 엔진 실행으로 자동화 실행 스크립트//
+
 -projectPath="C:\Users\taejin618\Katalon Studio\CheckUp\CheckUp.prj" -retry=0 -testSuitePath="Test Suites/Test_1" -browserType="Chrome" -executionProfile="default" -apiKey="f7739fa0-1934-40bd-a24a-4a48f2031974" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true
 
 -projectPath="C:\Users\taejin618\Katalon Studio\CheckUp\CheckUp.prj" -retry=0 -testSuitePath="Test Suites/Test Suite_1" -browserType="Chrome" -executionProfile="default" -apiKey="f2cc6a94-2a4a-4435-b15a-9c0911989ada" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true
